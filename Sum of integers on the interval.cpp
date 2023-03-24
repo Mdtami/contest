371c4957b71a3fecc83f1b78dd82e8e16eaccf05
Md. Tamim khan <mdtamimkhan842@gmail.comgit config --global user.name mdtamimkhan842omit --global to set the identity only in the repository$ git config --global user.email mdtamimkhan842@gmail.com>
2023-03-25 01:46:18 +0600

    second commit

--- Sum of integers on the interval.cpp
+++ Sum of integers on the interval.cpp
@@ -1,14 +1,31 @@
 #include <bits/stdc++.h>
 using namespace std;
+#define ordered_set tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update>
+#define faster                    \
+    ios_base::sync_with_stdio(0); \
+    cin.tie(0);                   \
+    cout.tie(0);
+#define mod 1000000007
+typedef long long int ll;
+typedef unsigned long long int llu;
 int main()
 {
-    unsigned long long int a,b,sum=0,i;
-    cin>>a>>b;
-    a = abs(a);
-    b = abs(b);
-    for( i=a; i<=b; i++)
+    string a, b;
+    cin >> a >> b;
+    llu i;
+    vector < ll >> v;
+    vector<ll> sum = 0;
+    if (a >= 0 && b >= 0)
     {
-        sum += i;
+        for (i = a; i <= b; i++)
+        {
+            v.push_back(i);
+            sum += v[i];
+        }
     }
-    cout<<sum<<endl;
+    for (i = 1; i <= 1; i++)
+    {
+        cout << sum[i] << endl;
+    }
 }
+// wrong answer
